0a2e2bd091d8e45ccb56f51a3b0f8f5b1122acb8
DavideBenedetti95 <davidebenedetti1995@gmail.com>
2023-07-03 18:01:57 +0200

    test stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -24,9 +24,9 @@ const Stats = () => {
   return (
     <motion.section ref={ref} animate={controls} variants={opacityVariants} initial='hidden' className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
       {stats.map((e)=>(
-        <div key={e.id} className={`flex-1 flex justify-start items-center flex-row ml-3`}>
+        <div key={e.id} className={`flex justify-start items-center flex-row ml-3`}>
           <h4 className='font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[50px] leading-[40px] text-white'>{e.value}</h4>
-          <p className='font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[25px] leading-[20px] text-gradient ml-3 uppercase'>{e.title}</p>
+          <p className='font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[25px] leading-[20px] text-gradient mx-5 uppercase'>{e.title}</p>
         </div>
       ))}
     </motion.section>
